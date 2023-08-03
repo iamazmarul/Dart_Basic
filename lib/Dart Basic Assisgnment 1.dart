892c6945bb353db5401426c32cf2e9bb29dcaa67
Azmarul Islam <imjohnny.dev@gmail.com>
2023-08-04 00:37:24 +0600

    Dart Basic Assisgnment - 1

---
+++ lib/Dart Basic Assisgnment 1.dart
@@ -0,0 +1,17 @@
+void main (){
+  const int a = 50;
+  const int b = 24;
+
+  int additionResult = a+b;
+  int subtractionResult = a-b;
+  int multiplicationResult = a*b;
+  double divisionResult = a/b;
+  int modulusResult = a%b;
+
+  print("a = $a, b = $b");
+  print("Addition: $a + $b = $additionResult");
+  print("Subtraction: $a - $b = $subtractionResult");
+  print("Multiplication: $a + $b = $multiplicationResult");
+  print("Division: $a + $b = $divisionResult");
+  print("Modulus: $a + $b = $modulusResult");
+}
