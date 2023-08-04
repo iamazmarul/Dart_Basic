b0561ee8a5447502debd63b56be524cf5efb0b32
Azmarul Islam <imjohnny.dev@gmail.com>
2023-08-04 23:15:21 +0600

    Live Test

---
+++ lib/Live Test Dart Basic.dart
@@ -0,0 +1,11 @@
+void main (){
+  Map<String, dynamic> persionList = {
+    "PersonID": 212472,
+    "Name": "Azmarul Islam",
+    "Age": 25,
+    "Salary": 32000,
+    "Gender": "Male"
+  };
+   print(persionList["Salary"]);
+
+}
