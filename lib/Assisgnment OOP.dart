218ca49b25b82de734e1c72f7c6e8cb03f620eb9
Azmarul Islam <imjohnny.dev@gmail.com>
2023-08-18 23:33:28 +0600

    Assignment OOP

--- lib/Assisgnment OOP.dart
+++ lib/Assisgnment OOP.dart
@@ -30,14 +30,14 @@ class Car {
 }
 
 void main(){
-  Car car1 = Car("Toyota", "Camry", 2020, 10000.0);
-  car1.drive(10000.0);
+  Car car1 = Car("Toyota", "Camry", 2020, 0);
+  car1.drive(10000);
 
-  Car car2 = Car("Honda", "Civic", 2018, 8000.0);
-  car2.drive(8000.0);
+  Car car2 = Car("Honda", "Civic", 2018, 0);
+  car2.drive(8000);
 
-  Car car3 = Car("Ford ", "F-150", 2015, 15000.0);
-  car3.drive(15000.0);
+  Car car3 = Car("Ford ", "F-150", 2015, 0);
+  car3.drive(15000);
 
   print("Car 1: ${car1.getBrand()} ${car1.getModel()} ${car1.getYear()} Miles: ${car1.getMilesDriven()} Age: ${car1.getAge()}");
   print("Car 2: ${car2.getBrand()} ${car2.getModel()} ${car2.getYear()} Miles: ${car2.getMilesDriven()} Age: ${car2.getAge()}");
